87218e4b29dd6af406491fb35980072782bc4a4c
Gleison <78481063+Gleison-Alves@users.noreply.github.com>
2021-05-26 22:16:45 -0300

    primeiro página

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+function Home(){
+    return(
+         <div>
+            <h1>Home 3</h1>
+            <Contador/>
+        </div>
+    )
+       
+    
+        
+    
+     
+}
+
+export default Home
+
+function Contador(){
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+
+    return(
+        <div>
+            <div>{Contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+        
+    
+}
