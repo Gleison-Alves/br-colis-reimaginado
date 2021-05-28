c39941ceee3cd742009f0860b5af25bce8a41c65
Gleison <78481063+Gleison-Alves@users.noreply.github.com>
2021-05-27 22:56:13 -0300

    agora com html e css

---
+++ pages/js/index.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+
+function Home(){
+return (
+    
+
+    <div>
+        <h2>Home</h2>
+        <Link href="/sobre">
+            <a>acesse o link sobre</a>
+        </Link>
+        
+    </div>
+
+
+)
+        
+}
+
+export default Home
